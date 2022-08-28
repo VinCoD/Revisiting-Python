d18b57608ce3c461b51a804a6d53e04a042f3142
VinCoD <5895.2020@students.ku.ac.ke>
2022-08-28 13:00:04 +0300

    Added more test cases

--- test_calc.py
+++ test_calc.py
@@ -6,14 +6,19 @@ from calc import add, subtract, multiply, divide
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        result = add(5, 10)
-        self.assertEqual(result, 15)
+        self.assertEqual(add(5, 10), 15)
+        self.assertEqual(add(-5, 10), 5)
+        self.assertEqual(add(-5, -10), -15)
 
     def test_subtract(self):
-        self.assertEqual(subtract(20,10), 10)
+        self.assertEqual(subtract(20, 10), 10)
+        self.assertEqual(subtract(-5, 10), -15)
+        self.assertEqual(subtract(-5, -10), 5)
 
     def test_multipy(self):
         self.assertEqual(multiply(20,10), 200)
+        self.assertEqual(multiply(-5, 10), -50)
+        self.assertEqual(multiply(-5, -10), 50)
 
     def test_divide(self):
         self.assertEqual(divide(20, 10), 2)
