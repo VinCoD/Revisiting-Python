ea1d4081ba0834e7449f9aad6fce25887e9c8652
VinCoD <5895.2020@students.ku.ac.ke>
2023-01-11 14:14:40 +0300

    Added Exception handling to the file program

--- files/files.py
+++ files/files.py
@@ -1,7 +1,9 @@
 name = input("Enter the name of the Document and its extension e.g. example.pdf: \n")
 
+try:
+    with open(name) as file:
+        content = file.read()
+    print(content)
 
-with open(name) as file:
-    content = file.read()
-
-print(content)
+except Exception:
+    print("-----!!!please check the file name and try again!!!-----")
