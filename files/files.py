38edca888e0d3d7a105c6d056accabce6d6d8519
VinCoD <5895.2020@students.ku.ac.ke>
2023-01-11 14:10:42 +0300

    Advanced the files program

--- files/files.py
+++ files/files.py
@@ -1,7 +1,7 @@
-x = 'words.txt'
+name = input("Enter the name of the Document and its extension e.g. example.pdf: \n")
 
 
-with open(x) as file:
+with open(name) as file:
     content = file.read()
 
 print(content)
