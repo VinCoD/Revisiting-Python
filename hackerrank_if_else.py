f6f3c43f39543005fad969d22a3bbe9f20d31fb5
VinCoD <5895.2020@students.ku.ac.ke>
2023-01-14 10:38:43 +0300

    Hackerrank if_elif

---
+++ hackerrank_if_else.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input("Enter n: \n").strip())
+    
+if(n%2 !=0):
+    print("Weird")
+    
+elif(n%2==0):
+    if(n>=2 and n<=5):
+        print("Not Weird")
+    elif(n>=6 and n<=20):
+        print("Weird")
+    elif(n>20):
+        print("Not Weird")
